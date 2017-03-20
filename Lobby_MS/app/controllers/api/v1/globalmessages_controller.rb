5e3f018d91ae7c7346888bea2e440d0c15e5ba06
Michael <mamurciac@unal.edu.co>
2017-03-20 01:51:12 -0500

    Adding Global and Normal messages' entities

---
+++ Lobby_MS/app/controllers/api/v1/globalmessages_controller.rb
@@ -0,0 +1,42 @@
+class GlobalmessagesController < ApplicationController
+	respond_to :json
+    skip_before_filter  :verify_authenticity_token
+
+    def show
+        respond_with GlobalMessage.find(params[:id])
+    end
+
+    # Creating globalmessages
+    def create
+        globalmessage=GlobalMessage.new(globalmessage_params)
+
+        if globalmessage.save
+            render json: globalmessage, status: 201
+        else
+            render json: { errors: globalmessage.errors}, status: 422
+        end
+    end
+
+    # Updating globalmessages
+    def update
+        globalmessage = GlobalMessage.find(params[:id])
+
+        if globalmessage.update(globalmessage_params)
+            render json: globalmessage, status: 200
+        else
+            render json: { errors: globalmessage.errors }, status: 422
+        end
+    end
+
+    # Deleting globalmessages
+    def destroy
+        globalmessage = GlobalMessage.find(params[:id])
+        globalmessage.destroy
+        head 204
+    end
+
+    private
+    def globalmessage_params
+        params.require(:globalmessage).permit(:idUser, :content)
+    end
+end
---
+++ Lobby_MS/app/helpers/normalmessages_helper.rb
@@ -0,0 +1,2 @@
+module NormalmessagesHelper
+end
